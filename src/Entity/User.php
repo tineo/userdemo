4b133524df2ba6f060430c73406449109adfb80d
Cesar Gutierrez <cesar@tineo.mobi>
2018-06-01 10:12:05 -0500

    initial d

---
+++ src/Entity/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+}
